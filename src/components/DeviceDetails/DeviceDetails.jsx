ee99525696da64ef786274fba13106f349ae58a7
EngrArfin <arfin.cse.green.edu.bd@gmail.com>
2023-05-16 22:16:37 +0600

    first commit

---
+++ src/components/DeviceDetails/DeviceDetails.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DeviceDetails = () => {
+    return (
+        <div>
+            <h1>Information About Details </h1>
+        </div>
+    );
+};
+
+export default DeviceDetails;
